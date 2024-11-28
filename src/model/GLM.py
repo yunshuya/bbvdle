2a4da8d33313c926e924816640aed3344f7ef33e
sunyia123 <18317128770@163.com>
2024-11-28 12:40:49 +0800

    modified:   src/model/GLM.py

--- src/model/GLM.py
+++ src/model/GLM.py
@@ -49,5 +49,5 @@ def reply():
 
 # 启动 Flask 服务
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-    # app.run(debug=True, host="0.0.0.0", port=5000)
+    # app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
